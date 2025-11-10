1caee610d1a23b4c04abdd3ea5e85f2172d3a4eb
Ameer8055 <155080912+Ameer8055@users.noreply.github.com>
2025-11-10 22:42:43 +0530

    css changes

--- fronted/src/pages/SignInPage.jsx
+++ fronted/src/pages/SignInPage.jsx
@@ -3,7 +3,7 @@ import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
 import './SignInPage.css';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL  ;
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api' ;
 
 const SignInPage = ({ onLogin }) => {
   const navigate = useNavigate();
